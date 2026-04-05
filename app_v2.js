4f30d14cb274a25f85d46dad2f5b442ea168b52c
arieltecnologia777-cmd <arieltecnologia777@gmail.com>
2026-04-05 15:34:33 -0500

    Update app_v2.js

--- app_v2.js
+++ app_v2.js
@@ -167,7 +167,7 @@ async function cargarDatosModulo() {
   //    • KV entrega el fileIdReal verdadero y el estado
   for (const a of listaOD) {
 
-    const registro = listaKV.find(k => k.fileId && k.fileId.split(".").pop() === a.id);
+    const registro = listaKV.find(k => k.fileId && k.fileId.includes(a.id));
 
     if (registro) {
 
